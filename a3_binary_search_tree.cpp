cea3c9db825d5e2770fdcb792af7d12be188659a
Jimmy Xu <xujimmyy@gmail.com>
2017-03-09 00:13:28 -0500

    7/8 cases

--- a3_binary_search_tree.cpp
+++ a3_binary_search_tree.cpp
@@ -1,4 +1,5 @@
- #include <iostream>
+#include <iostream>
+#include <stack>
 #include "a3_binary_search_tree.hpp"
 
 using namespace std;
@@ -36,7 +37,7 @@ BinarySearchTree::~BinarySearchTree(){
 	if (root_){
 		deleteTree(root_);	
 	}
-	
+	root_=NULL;
 }
 
 // Returns the number of nodes in the tree. 
@@ -61,15 +62,15 @@ DataType BinarySearchTree::min() const{
 	}
 	return cur->val;
 }
+
 // Returns the maximum depth of the tree. A tree with only the root node has a 
 // depth of 0. You can assume that this function will never be called on an 
 // empty tree.
 
 int BinarySearchTree::getNodeDepth(Node* n) const{
-	//value = n->value
 	int nodeDepth=0;
 	Node* cur = root_;
-	while (&cur!=&n){
+	while (cur->val!=n->val){
 		if (cur->val>n->val){
 			//left
 			cur=cur->left;
@@ -83,23 +84,25 @@ int BinarySearchTree::getNodeDepth(Node* n) const{
 	return nodeDepth;
 }
 
-//int recurDepth (BinarySearchTree::Node* cur) {
-//	if (cur->left){
-//		recurDepth (cur->left);
-//	}
-//	if (cur->right){
-//		recurDepth (cur->right);
-//	}
-//	return BinarySearchTree::getNodeDepth(cur);
-//}
-
 unsigned int BinarySearchTree::depth() const{
-	Node* cur = root_;
-	int maxDepth=0;
-	
-	
-	//return recurDepth(root_);
+	Node* cur=root_;
+	stack<Node *> s;
+	int maxD=0;
+	s.push(root_);
+	while (!s.empty()){
+		if (cur->left){
+			s.push(cur->left);
+		}
+		if (cur->right){
+			s.push(cur->right);
+		}
+		maxD=std::max(maxD,getNodeDepth(cur));
+		cur=s.top();
+		s.pop();
+	}
+	return maxD;
 }
+
 // You can print the tree in whatever order you prefer. However, methods such 
 // as in-order or level-order traversal could be the most useful for debugging.
 
@@ -117,23 +120,29 @@ void recurPrint(BinarySearchTree::Node* cur) {
 }
 
 void BinarySearchTree::print() const{
-	recurPrint(root_);
+	if (root_){
+		recurPrint(root_);
+	}else{
+		cout<<"Empty tree";
+	}
 	cout<<endl;
 }
 // Returns true if a node with the value val exists in the tree; otherwise, 
 // it returns false.
 bool BinarySearchTree::exists(DataType val) const{
+	if (!root_){
+		return false;
+	}
 	Node* cur = root_;
 	while (cur->val!=val){
-		if (!cur->left&&!cur->right){
-			return false;
-		}
 		if (cur->left&&cur->val>val){
 			//left
 			cur=cur->left;
 		}else if (cur->right&&cur->val<val){
 			//right
 			cur=cur->right;
+		}else{
+			return false;
 		}
 	}
 	return true;
@@ -150,35 +159,41 @@ BinarySearchTree::Node** BinarySearchTree::getRootNodeAddress(){
 // Inserts the value val into the tree. Returns false if val already exists in 
 // the tree, and true otherwise.
 bool BinarySearchTree::insert(DataType val){
+	if (exists(val)){
+		return false;
+	}
 	Node* newNode=new Node(val);
 	if (!root_){
 		root_=newNode;
 		size_++;
+		print();
 		return true;
 	}
 	Node* cur = root_;
-	while (cur->left||cur->right){
-		if (cur->left->val==val || cur->right->val==val){
-			return false;
-		}
-		if (cur->left&&cur->val>val){
+	while (true){
+		if (cur->left && cur->val>val){
 			//left
 			cur=cur->left;
-		}else if (cur->right&&cur->val<val){
+		}else if (cur->right && cur->val<val){
 			//right
 			cur=cur->right;
+		}else{
+			break;
 		}
 	}
 	if (cur->val>val){
+		
 		//left
 		cur->left=newNode;
+		size_++;
+		print();
 		return true;
 	}
 	//right
 	cur->right=newNode;
 	size_++;
+	print();
 	return true;
-
 }
 // Removes the node with the value val from the tree. Returns true if successful, 
 // and false otherwise.
@@ -196,7 +211,8 @@ bool BinarySearchTree::remove(DataType val){
 		//root is val
 		if (size_==1){
 			size_--;
-			delete this;
+			delete root_;
+			root_=NULL;
 			return true;
 		} else if (!cur->left){
 			// no left, must have a right, since size>1
@@ -222,6 +238,9 @@ bool BinarySearchTree::remove(DataType val){
 				cur->right=removeNode->left;
 				delete removeNode;
 				removeNode=NULL;
+			}else {
+				delete cur->right;
+				cur->right=NULL;
 			}
 		}
 		size_--;
@@ -255,11 +274,19 @@ bool BinarySearchTree::remove(DataType val){
 			pVal->left=removeNode->left;
 		}else{ // both left and right
 			//find parent of max of left subtree
-			pVal=removeNode->left;
+			//THIS IS THE PART THAT DOESNT WORK
+			//if removeNode->left is a leaf this doesnt work
+			//presumably also the case for !childLeft
+			
+			if (removeNode->left->right){
+				pVal=removeNode->left;
+			}else{
+				pVal=removeNode;
+			}
+			
 			while(pVal->right->right){
 				pVal=pVal->right;
 			}
-			// cur is parent of max left
 			
 			removeNode->val=pVal->right->val;
 			removeNode=pVal->right;
@@ -288,10 +315,13 @@ bool BinarySearchTree::remove(DataType val){
 				pVal->right=removeNode->left;
 			}
 		}
+	}
 	delete removeNode;
 	removeNode=NULL;
+	size_--;
+	return true;
 	//if has left and right	
-	}
+	
 }
 // Update the avlBalance starting at node n
 void BinarySearchTree::updateNodeBalance(Node* n){
