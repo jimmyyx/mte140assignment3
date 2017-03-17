66b37e7ee41c5d80ca88c314b36ccf1027457b08
Jimmy Xu <xujimmyy@gmail.com>
2017-03-17 18:29:21 -0400

    Add files via upload

--- a3_avl_tree.cpp
+++ a3_avl_tree.cpp
@@ -7,13 +7,69 @@ using namespace std;
 
 typedef BinarySearchTree::DataType DataType;
 
-AVLTree::AVLTree () {
+AVLTree::Node* AVLTree::parentOf (Node* n){
+	if (getRootNode()==n){
+		return n;
+	}
+	Node* cur = getRootNode();
+	while ((cur->left->val != n->val) && (cur->right->val != n->val)){
+		cur->val > n->val ? cur=cur->left : cur=cur->right;
+	}
+	return cur;
+}
+
+void AVLTree::L (Node* n){
 	
 }
 
-unsigned int AVLTree::depth(Node* n) const{
-	BinarySearchTree::Node* cur;
-	queue<BinarySearchTree::Node *> q;
+void AVLTree::R (Node* n){
+	
+}
+
+void AVLTree::LR (Node* n){
+	
+}
+
+void AVLTree::RL (Node* n){
+	
+}
+
+
+AVLTree::Node* AVLTree::nodePointer(DataType val) {
+	Node* cur = getRootNode();
+	while (cur->val!=val){
+		if (cur->left&&cur->val>val){
+			//left
+			cur=cur->left;
+		}else if (cur->right&&cur->val<val){
+			//right
+			cur=cur->right;
+		}
+	}
+	return cur;
+}
+
+int AVLTree::getNodeDepth(Node* n) {
+	int nodeDepth=0;
+	Node* cur = getRootNode();
+	while (cur->val!=n->val){
+		if (cur->val>n->val){
+			//left
+			cur=cur->left;
+			nodeDepth++;
+		}else if (cur->val<n->val){
+			//right
+			cur=cur->right;
+			nodeDepth++;
+		}
+	}
+	return nodeDepth;
+}
+
+int AVLTree::depth(Node* n) {
+	
+	Node* cur;
+	queue<Node *> q;
 	int maxD=0;
 	q.push(n);
 
@@ -31,14 +87,31 @@ unsigned int AVLTree::depth(Node* n) const{
 	return maxD;
 }
 
-unsigned int hDiff (BinarySearchTree::Node* left, BinarySearchTree::Node* right){
-	return depth(left)-depth(right);
+int AVLTree::hDiff (Node* n){	
+	if (n->left){
+		if (n->right){
+			return depth(n->left)-depth(n->right);
+		}
+		return depth(n->left);
+	}
+	if (n->right){
+		return 0-depth(n->right);
+	}
+	return 0;
 }
 
 bool AVLTree::insert(DataType val){
-	BinarySearchTree::insert(val);
+	if (!BinarySearchTree::insert(val)){
+		return false;
+	}
+	
+	if (hDiff(nodePointer(val)) < -1){
+		//rotate left
+	}else if  (hDiff(nodePointer(val)) > 1){
+		//rotate right
+	}
 }
 
 bool AVLTree::remove(DataType val){
-	BinarySearchTree::remove(val);
+	return BinarySearchTree::remove(val);
 }
--- a3_avl_tree.hpp
+++ a3_avl_tree.hpp
@@ -6,15 +6,29 @@
 class AVLTree: public BinarySearchTree
 {
 public:
+	typedef int DataType;
+	
 	// Overriden insert and remove functions 
 	// Do not modify these definitions
     bool insert(DataType val);
     bool remove(DataType val);
+    
+
 
 	// Define additional functions and attributes that you need below
-	AVLTree();
+	int getNodeDepth(Node* n);
+	int hDiff (Node* n);
+	int depth(Node* n);
 	
-	unsigned int depth(Node* n) const;
+	void L (Node* n);
+	void R (Node* n);
+	void LR (Node* n);
+	void RL (Node* n);
+	Node* nodePointer (DataType val) ;
+	Node* parentOf (Node* n);
+	
+private:
+	 friend class BinarySearchTree;
 };
 #endif
 
